f9dd0bca3ee2a4d1795cdb673854a1a90ca60c0c
Ilya Borovitinov <icehaunter@gmail.com>
2020-07-18 14:00:57 +0300

    Initialized a repro repository

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <h1>{{ newMsg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script setup="props" lang="ts">
+import { computed, ref, watchEffect } from "vue"
+
+declare const props: {
+  msg: string
+}
+
+export const newMsg = computed(() => props.msg + '!!!')
+export const count = ref(0)
+
+watchEffect(() => console.log(count.value))
+</script>
+
+
